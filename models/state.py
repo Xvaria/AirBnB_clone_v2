8c219ce8925ba142fd90e0d5615fed1ccbfc9677
Xvaria <2349@holbertonschool.com>
2021-03-29 21:58:19 +0000

    ADDED | New class attributes and relationships with ORM

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.engine.file_storage import FileStorage
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        kwargs = {"cascade": "all, delete-orphan", "backref": "state"}
+        cities = relationship("City", **kwargs)
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id"""
+            objDict = FileStorage.all()
+            citiesList = []
+            for key, value in objDict:
+                if value.state_id == self.id:
+                    citiesList.append(value)
+            return (citiesList)
