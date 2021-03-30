fcac7e80510c15242f5242f0ca419d595c8afa91
DiegoCol93 <dfelo93@gmail.com>
2021-03-29 19:59:38 -0500

    REMOVED | Environment conditional and extra attributes.

--- models/user.py
+++ models/user.py
@@ -7,13 +7,10 @@ import os
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    # kwargs = {"cascade": "all, delete-orphan", "backref": "user"}
+    # reviews = relationship("Review", **kwargs)
