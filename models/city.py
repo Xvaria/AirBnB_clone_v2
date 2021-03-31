640196d705733b480ca529af744c280aec4f5f6e
DiegoCol93 <dfelo93@gmail.com>
2021-03-30 20:59:17 -0500

    FIXED | Positioning of default value variables.

--- models/city.py
+++ models/city.py
@@ -9,12 +9,11 @@ from os import getenv as env
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
+    place_id = ""
+    user_id = ""
+    text = ""
     if env("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         kwargs = {"cascade": "all, delete-orphan", "backref": "cities"}
         places = relationship("Place", **kwargs)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
