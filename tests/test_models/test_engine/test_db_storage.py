7c070cbe256c5f2ce4bd02cca4efdb8ab3280ef6
DiegoCol93 <dfelo93@gmail.com>
2021-03-30 21:06:15 -0500

    ADDED | Mock environment variables.

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ Module for testing DBstorage """
-import unittest
+from unittest import TestCase, mock
+import os
 
-
-class test_DBStorage(unittest.TestCase):
-    pass
+class test_DBStorage(TestCase):
+    """  """
+    @mock.patch.dict(os.environ, {
+        "HBNB_ENV": "test",
+        "HBNB_MYSQL_USER": "hbnb_test",
+        "HBNB_MYSQL_PWD": "hbnb_test_pwd",
+        "HBNB_MYSQL_HOST": "localhost",
+        "HBNB_MYSQL_DB": "hbnb_test_db",
+        "HBNB_TYPE_STORAGE": "db",
+    })
+    def test_00(self):
+        pass
