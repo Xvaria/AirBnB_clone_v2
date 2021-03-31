282d08fe45122680cd6a4fcc6a40858f6a50ea70
DiegoCol93 <dfelo93@gmail.com>
2021-03-30 20:59:00 -0500

    FIXED | Positioning of default value variables.

--- models/review.py
+++ models/review.py
@@ -8,11 +8,10 @@ from os import getenv as env
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
+    place_id = ""
+    user_id = ""
+    text = ""
     if env("HBNB_TYPE_STORAGE") == "db":
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
