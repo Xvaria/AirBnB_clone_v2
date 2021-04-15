b7824b7cc8bea9b9d2a84586d808b9c5009ee614
Xvaria <2349@holbertonschool.com>
2021-04-15 22:23:52 +0000

    Ready | Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''generates a .tgz archive from the contents of the web_static\
+folder of your AirBnB Clone repo, using the function do_pack'''
+from fabric.api import local, put, run, env
+from os import path
+from datetime import datetime
+
+env.hosts = ["{}@35.196.187.53".format(env.user),
+             "{}@34.75.244.141".format(env.user)]
+
+def do_pack():
+    '''Packing web_static to versions/web_static_20170314233357.tgz'''
+    if not path.exists("versions"):
+        local("mkdir versions")
+    local("tar -czf versions/web_static_{}.tgz web_static".
+          format(str(datetime.now()).split(".")[0].replace("-", "").
+                 replace(" ", "").replace(":", "")))
+
+def do_deploy(archive_path):
+    '''distributes an archive to your web servers'''
+    if not path.exists(archive_path):
+        return(False)
+
+    try:
+        # upload tgz file to servers
+        put(archive_path, "/tmp/")
+
+        # variables to be used
+        tgzname = archive_path.split("/")[-1]
+        path = "/data/web_static/releases/{}/".format(tgzname[:-4])
+        tmpath = "/tmp/{}".format(tgzname)
+        sympath = "/data/web/static/current"
+
+        # create folder to tgz file
+        run("mkdir -p {}".format(path))
+
+        # uncompressed tgz file
+        run("tar -xzf {} -C {}".format(tmpath, path))
+
+        # remove tgz
+        run("rm {}".format(tmpath))
+
+        # move all uncompressed files
+        run("mv {}web_static/* {}".format(path, path))
+
+        # remove the symbolic link
+        run("rm -rf {}".format(sympath))
+
+        # create new symbolic link
+        run("ln -s {} {}".format(path, sympath))
+        return(True)
+
+    except:
+        return(False)
