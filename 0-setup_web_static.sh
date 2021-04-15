60ccbfb727768434d345100f704df42781933d7e
Xvaria <2349@holbertonschool.com>
2021-04-15 19:45:44 +0000

    Ready | Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,7 +11,7 @@ rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 str="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-if [ $(grep -c "location /hbnb_static {" /etc/nginx/sites-available/default) -eq 0 ]; then
+if [ "$(grep -c "location /hbnb_static {" /etc/nginx/sites-available/default)" -eq 0 ]; then
     sed -i "45i $str" /etc/nginx/sites-available/default
 fi
 service nginx restart
