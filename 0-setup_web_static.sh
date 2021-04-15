27714dc4ec412d55dab21ded476ca1575d7d741f
DiegoCol93 <dfelo93@gmail.com>
2021-04-15 09:39:14 -0500

    |  ADDED | Exit at the end of script.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 # Install nginx. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|
 #
 # Check if nginx default config file exists.
-if [[ ! -e /etc/nginx/sites-available/default ]]; then
+if ! which nginx > /dev/null; then
     # update and install if no file exists.
     apt-get update && apt-get install nginx -y
 fi
@@ -35,14 +35,15 @@ fi
 # Create symlink file again.
 ln -s /data/web_static/releases/test/ /data/web_static/current
 
-# Change user and group of /data/*.  - - - - - - - - - - - - - - - - - - - - - |
-chown -R ubuntu:ubuntu /data/
+# Change user and group of /data/*.  - - - - - - - - - - - - - - - - - - - - -|
+chown -RL ubuntu:ubuntu /data/
 
 # Add /hbnb_static location on nginx's default file. - - - - - - - - - - - - -|
-# if ! grep -q "hbnb_static" /etc/nginx/sites-available/default; then
-#     Line='\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}'
-#     sed -i "37i $Line" /etc/nginx/sites-available/default
-# fi
+if ! grep -q "hbnb_static" /etc/nginx/sites-available/default; then
+    Line='\\n\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}'
+    sed -i "37i $Line" /etc/nginx/sites-available/default
+fi
 
 # Restart nginx's service. - - - - - - - - - - - - - - - - - - - - - - - - - -|
 service nginx restart
+exit 0
