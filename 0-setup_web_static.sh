60b0c5feae9e8185541ff13261fe3e640918c338
DiegoCol93 <dfelo93@gmail.com>
2021-04-15 09:52:29 -0500

    |  FIXED  | Inclusion of the echo command into if statement.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,16 +15,16 @@ if [[ ! -e /data/web_static/releases/test ]]; then
 fi
 if [[ ! -e /data/web_static/shared ]]; then
     mkdir -p /data/web_static/shared
+    # Create dummy .html file - - - - - - - - - - - - - - - - - - - - - - - - |
+    echo "<html>
+      <head>
+      </head>
+      <body>
+        Holberton School
+      </body>
+    </html>" > /data/web_static/releases/test/index.html
 fi
 
-# Create dummy .html file - - - - - - - - - - - - - - - - - - - - - - - - - - |
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
 
 # Create symbolic link. - - - - - - - - - - - - - - - - - - - - - - - - - - - |
 #
