c445be645648cb33ea62e0283bb247b974432314
Xvaria <2349@holbertonschool.com>
2021-05-02 22:35:36 +0000

    UPDATED | Add new method call close

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,7 @@ class FileStorage:
                 self.__objects.pop(objDict)
             except:
                 pass
+
+    def close(self):
+        '''call reload() method for deserializing the JSON file to objects'''
+        self.reload()
