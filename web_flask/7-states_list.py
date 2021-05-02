6d58353652944bd9ae6f93ecec1e13fc2d754bbd
Xvaria <2349@holbertonschool.com>
2021-05-02 23:37:40 +0000

    Ready | List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Start flask app web, using storage engine """
+from models import storage
+from flask import render_template
+from models.state import State
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """ Index to display lists """
+    states = storage.all(State)
+    states_list = {}
+    for key, value in states.items():
+        states_list[value.name] = value
+    return render_template('7-states_list.html', states_list=states_list)
+
+
+@app.teardown_appcontext
+def close(states):
+    ''' Close the connection with db '''
+    if states is not None:
+        storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
