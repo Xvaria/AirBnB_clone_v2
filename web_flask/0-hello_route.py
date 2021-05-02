927323d0ad28e573a947a5adaf9c59553f34aa8d
Xvaria <2349@holbertonschool.com>
2021-05-02 03:32:06 +0000

    Ready | Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,13 @@ from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
 @app.route("/")
 def home():
-        '''Show index to web page'''
-        return("Hello HBNB!")
+    '''Show index to web page'''
+    return("Hello HBNB!")
+
 
 if __name__ == "__main__":
-	app.run(host = "0.0.0.0")
+    app.run(host="0.0.0.0")
